abbb574cecd4e668c300bdfdd7592f2125194cda
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-08-24 21:07:20 +0600

    static function

---
+++ static-function.js
@@ -0,0 +1,13 @@
+// static function k call korte hole class name dhore call korte hobe...object dhore call kora jabe naa
+
+class Person {
+  constructor() {
+    this.firstName = 'Ehsanul';
+    this.lastName = 'Islam';
+  }
+  static fullName() {
+    return `I am a static function`;
+  }
+}
+
+console.log(Person.fullName());
